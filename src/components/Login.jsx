614403ca131f2a04d1f41a509bc6f1486a54bfe5
Juanm42305 <jufemasa0523@gmail.com>
2025-10-06 17:05:31 -0500

    Agrego componente Login.jsx base

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Pantalla de Login</h1>
+      <p>Componente base funcionando correctamente ✅</p>
+    </div>
+  );
+};
+
+export default Login;
