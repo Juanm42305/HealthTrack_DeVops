7fe47085c78c355360ac96391dfb173900fc07d3
Juancho <jufemasa0523@gmail.com>
2025-11-03 18:37:42 -0500

    mensaje del commit

--- backend/server.js
+++ backend/server.js
@@ -739,6 +739,6 @@ app.put('/api/doctor/appointments/:appointmentId/finish', async (req, res) => {
 
 
 // --- INICIAR SERVIDOR ---
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
